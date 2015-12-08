7da83fac03aaa053f1f2e00598c061cd07774a88
Philippe-Antoine Lehoux <plehoux@gmail.com>
2015-12-08 15:17:55 -0500

    First attempt at background event

--- export.rb
+++ export.rb
@@ -29,6 +29,33 @@ groups = data.map.with_index { |item, i|
   }
 }
 
+# # background items
+
+items << {
+  content: 'SMTP is introduced',
+  start: '1980',
+  end: '1981',
+  eventType: 'background',
+  type: 'background',
+}
+
+items << {
+  content: 'POP3 and IMAP4 are introduced',
+  start: '1996',
+  end: '1998',
+  eventType: 'background',
+  type: 'background',
+}
+
+items << {
+  content: 'INBOX Zero is introduced by Merlin Mann',
+  start: '2007',
+  end: '2008',
+  eventType: 'background',
+  type: 'background',
+}
+
+
 # Export
 File.write('items.js', "var items = #{JSON.dump(items)};")
 File.write('groups.js', "var groups = #{JSON.dump(groups)};")
