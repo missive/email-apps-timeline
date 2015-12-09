ebb020bc3eda83b67fabd5bdb60f1f7c79474f1b
Philippe-Antoine Lehoux <plehoux@gmail.com>
2015-12-09 12:53:04 -0500

    12.months fading

--- export.rb
+++ export.rb
@@ -27,7 +27,7 @@ items = data.flat_map.with_index do |item, i|
         id: "#{i + 1}-#{item['events'].length + 1}",
         group: i + 1,
         start: to_date(Time.now),
-        end: to_date(Time.now + 6.months),
+        end: to_date(Time.now + 12.months),
         eventType: event['type'] || 'generic',
         className: (event['type'] || 'generic') + ' fading',
       }
